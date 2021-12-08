aed63200e3d3fb779e179e982e4a88f893b83780
Angel Casado <acasadoalonso@gmail.com>
2021-12-08 22:12:52 +0100

    adjust the VM provisioning

--- VM/bootstrap.sh
+++ VM/bootstrap.sh
@@ -3,9 +3,19 @@ echo "==========================================================================
 echo " provisioning the VM machine"
 echo " ==========================="
 echo "============================================================================"
-apt-get update
-apt-get upgrade
-apt-get install -y neofetch git
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y neofetch git
+mkdir -p home/vagrant/src
+cd /home/vagrant/src
+pwd
+rm -r RealTimeScoring RTSsrc
+git clone https://github.com/acasadoalonso/RealTimeScoring.git
+ln -s RealTimeScoring RTSsrc
+cd RTSsrc
+sudo chown vagrant:vagrant -R . *
+sudo chmod 775 -R . *
+bash install.sh
 echo "============================================================================"
 echo " end of provisioning the VM machine"
 echo " =================================="
