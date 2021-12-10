bbb1c9f1e3a100e02b0349772b2cab8242f32d67
Angel Casado <acasadoalonso@gmail.com>
2021-12-10 12:01:42 +0100

    sync

--- VM/bootstrap.sh
+++ VM/bootstrap.sh
@@ -6,6 +6,7 @@ echo "==========================================================================
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y neofetch git
+neofetch
 mkdir -p /home/vagrant/src
 cd       /home/vagrant/src
 pwd
@@ -13,12 +14,15 @@ if [ -d RealTimeScoring ]
 then
     rm -r RealTimeScoring RTSsrc
 fi
+echo "=================="
 echo "Clone the RTS repo"
+echo "=================="
 git clone https://github.com/acasadoalonso/RealTimeScoring.git
 ln -s RealTimeScoring RTSsrc
 cd RTSsrc
 sudo chown vagrant:vagrant -R . *
 sudo chmod 775 -R . *
+echo "============================"
 echo "Install now the RTS software"
 echo "============================"
 echo " "
@@ -28,6 +32,8 @@ echo " end of provisioning the VM machine"
 echo " =================================="
 echo "============================================================================"
 hostnamectl
+neofetch
 echo "reboot now to get the version of the operating system"
+echo "<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 sudo reboot
 
